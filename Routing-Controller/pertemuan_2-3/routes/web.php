a1b5b48b52db76c4f1b22afb13a61c8a489a6f6d
BagusRezky <bagusrezky15@gmail.com>
2023-02-26 18:59:22 +0700

    02_praktikum_web_lanjut_tiga

--- Routing-Controller/pertemuan_2-3/routes/web.php
+++ Routing-Controller/pertemuan_2-3/routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ContactController;
+use App\Http\Controllers\NewsController;
+use App\Http\Controllers\ProgramController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +17,30 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::get('/', function(){
+    return view ('home');
 });
+
+Route::prefix('/category') -> group(function($index){
+    Route::get('/', [CategoryController::class, 'index']);
+    Route::get('/{index}', [CategoryController::class, 'show']);
+});
+
+Route::prefix('/news') -> group(function($index){
+    Route::get('/', [NewsController::class, 'index']);
+    Route::get('/{index}', [NewsController::class, 'show']);
+});
+
+Route::prefix('/program') -> group(function($index){
+    Route::get('/', [ProgramController::class, 'index']);
+    Route::get('/{index}', [ProgramController::class, 'show']);
+});
+
+Route::get('/about us', function(){
+    return view ('about');
+});
+
+Route::resource('/contact', ContactController::class)->only('index');
