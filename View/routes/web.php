9bea1b9b2ad2302df84786836ce47d81933fef2b
BagusRezky <bagusrezky15@gmail.com>
2023-02-27 15:35:11 +0700

    jobsheet 3 praktikum 2

--- View/routes/web.php
+++ View/routes/web.php
@@ -22,10 +22,16 @@ Auth::routes();
 
 // Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::get('/home',function(){return view('home');})->name('home');
-Route::get('/about-us',function(){return view('about-us');})->name('about-us');
-Route::get('/contact-us',function(){return view('contact-us');})->name('contact-us');
-Route::get('/news',function(){return view('news');})->name('news');
-Route::get('/product',function(){return view('product');})->name('product');
-Route::get('/program',function(){return view('program');})->name('program');
+Route::get('/home',function(){
+    return view('home');})->name('home');
+Route::get('/about-us',function(){
+    return view('about-us');})->name('about-us');
+Route::get('/contact-us',function(){
+    return view('contact-us');})->name('contact-us');
+Route::get('/news',function(){
+    return view('news');})->name('news');
+Route::get('/product',function(){
+    return view('product');})->name('product');
+Route::get('/program',function(){
+    return view('program');})->name('program');
 
